9d789fd5bad402dd220146e923970a1d69cbab0e
David Haylock <david.haylock@watershed.co.uk>
2018-08-21 14:34:03 +0100

    * Main File now sets the GL version depending on the Build environment

--- PCprototype/src/main.cpp
+++ PCprototype/src/main.cpp
@@ -3,11 +3,17 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(640,960,OF_WINDOW);			// <-------- setup the GL context
-
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
+#ifdef TARGET_OPENGLES
+    ofGLESWindowSettings settings;
+    settings.glesVersion = 2;
+#else
+    ofGLWindowSettings settings;
+    settings.setGLVersion(3,2);
+#endif
+    settings.setSize(640,960);
+    settings.windowMode = OF_WINDOW;
+    
+    ofCreateWindow(settings);
 	ofRunApp(new ofApp());
 
 }
